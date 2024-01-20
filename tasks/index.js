6d48280f6f3574cf05508b7e6153d45daaec4144
Niraj Kamdar <nirajkamdar27@pm.me>
2024-01-20 22:44:05 +0530

    feat: add all necessary tasks

--- tasks/index.js
+++ tasks/index.js
@@ -1,2 +1,2 @@
-exports.setupMessenger = require("./01_setupMessenger.js")
-exports.sendBorrowMessage = require("./02_sendBorrowMessage.js")
+exports.setupMessenger = require("./legacy_2/01_setupMessenger.js")
+exports.sendBorrowMessage = require("./legacy_2/02_sendBorrowMessage.js")
